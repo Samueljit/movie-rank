5dbb30b6127cac5c8eee10a43ec53f4fb4a34946
Samuel Infante <samueljit@gmail.com>
2022-02-23 22:51:28 -0300

    fix: add title validation in the same controller and 400 status code with a test and cath

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,27 +5,35 @@ import { request, response } from 'express';
 
 export const movieGet = async (req = request, res = response) => {
 
-    const queryTitle = req.query.title;
-    const jwt = process.env.API_V4_TMDB;
-    const url = process.env.MOVIE_SEARCH_URL;
+    try {
 
-    axios.get(url, {
-        headers: {'Authorization': `Bearer ${jwt}`},
-        params: {
+      const queryTitle = req.query.title;
+      const jwt = process.env.API_V4_TMDB;
+      const url = process.env.MOVIE_SEARCH_URL;
+
+      if (!queryTitle) {
+        res.status(400).json({
+          message: 'The title is required'
+        })
+      }
+
+      axios.get(url, {
+          headers: {'Authorization': `Bearer ${jwt}`},
+          params: {
           query: queryTitle
-        }
-      })
-      .then(response => {
-          let results = response.data.results;
-          let responseBody = results.map(element => {
-            const { title, overview, release_date, poster_path } = element;
-            return { title, overview, release_date, poster_path};
-          });
-          
-          res.status(200).json(responseBody);
-      
-      })
-      .catch(error => {
-        console.log(error);
-      })
+          }
+        })
+        .then(response => {
+            let results = response.data.results;
+            let responseBody = results.map(element => {
+              const { title, overview, release_date, poster_path } = element;
+              return { title, overview, release_date, poster_path};
+            });
+            
+            res.status(200).json(responseBody);
+        
+        })
+    } catch (error) {
+      console.log(error);
+    }
 }
