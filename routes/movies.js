7984a8e556d8d9e9d457ff1d84f35db94a85c4aa
Gustavo Bolivar <gustavobolivar5691@gmail.com>
2022-03-11 22:37:53 -0300

    fix: Standardized controller methods

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,9 @@
-import { movieGet } from '../controllers/movies.js';
+import { moviesGet } from '../controllers/movies.js';
 import { Router } from 'express';
 import { titleRequired } from '../middlewares/validate-fields.js';
 
 const router = Router();
 
-router.get('/search', [titleRequired], movieGet);
+router.get('/search', [titleRequired], moviesGet);
 
 export default router;
