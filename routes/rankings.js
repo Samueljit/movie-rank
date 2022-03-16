26008b4db78be19ca7a962503af0e13984b9671b
Samuel Infante <samueljit@gmail.com>
2022-03-15 23:34:52 -0300

    feat: Create the endpoint rankingsPut and opdate the rankings models so that the _v is not displayed in the model

--- routes/rankings.js
+++ routes/rankings.js
@@ -1,6 +1,7 @@
 import { rankingsGet,
   rankingsGetAll,
-  rankingsPost } from '../controllers/rankings.js';
+  rankingsPost,
+  rankingsPut } from '../controllers/rankings.js';
 import { check } from 'express-validator';
 import Router from 'express';
 import { validateFields } from '../middlewares/validate-fields.js';
@@ -22,5 +23,12 @@ router.get('/:id', [
   check('id','The id is invalid').isMongoId(),
   validateFields
 ], rankingsGet);
+
+router.put('/:id', [
+  validateJWT,
+  check('movies', 'The field movies is required').not().isEmpty(),
+  check('movies', 'The field movies must have a length of 10').isLength({ min: 10 }),
+  validateFields
+], rankingsPut);
 
 export default router;
